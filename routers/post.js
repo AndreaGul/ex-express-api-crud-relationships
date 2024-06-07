833260a7d754f12524b28a544f80f427f679ad3d
AndreaGul <andreagulino99@gmail.com>
2024-06-07 13:01:24 +0200

    feat:validator

--- routers/post.js
+++ routers/post.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router= express.Router();
 const {store, show ,index ,update,destroy}= require("../controllers/post");
+const validator = require("../middlewares/validator");
 
-router.post('/', store);
+
+router.post('/',validator, store);
+
 router.get('/:slug', show);
+
 router.get('/', index);
+
 router.post('/:slug', update);
+
 router.delete('/:slug', destroy);
 
 module.exports= router ;
